ec4b393aa0c4008f0a7b75df4561948151ff5b4f
Yeremy <yeremyvegap@gmail.com>
2025-11-06 22:39:48 -0500

    feat: relacion muchos a uno agregada (FK)

--- src/models/libro.py
+++ src/models/libro.py
@@ -1,20 +1,36 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from base import Base
 class Libro(Base):
 
     """tabla 'libros'
-    - id: PRIMARY KEY AUTOINC
+    - id_libro: PRIMARY KEY AUTOINC
+    - categoria_id: FOREIGN KEY de categorias(id_categoria)
+    - categoria : Relacion muchos(libros) a uno (categorias)
     - titulo : TEXT
     - autor: TEXT
     - precio: FLOAT
     """
     __tablename__ = "libros"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_libro = Column(Integer, primary_key=True, autoincrement=True)
+    
+    # FK hacia categorias
+    categoria_id = Column(Integer, ForeignKey("categorias.id"))
+    
+    # relacion inversa
+    categoria = relationship("Categoria", back_populates="libros")
+
     titulo = Column(String, nullable=False)
     autor = Column(String, nullable=False)
     precio = Column(Float, nullable=False)
 
     def __repr__(self) -> str:
-        return f"""<Libro id ={self.id} titulo='{self.titulo}' 
-        autor='{self.autor}' precio={self.precio:.2f} >"""
+        return f"""
+        <Libro id_libro ={self.id_libro} 
+        categoria_id ={self.id_categoria_id} 
+        categoria ={self.id_categoria} 
+        titulo='{self.titulo}' 
+        autor='{self.autor}' 
+        precio={self.precio:.2f} >
+        """
