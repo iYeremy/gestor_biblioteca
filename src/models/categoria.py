4204770840cef20614ef5475b7138955bd407757
Yeremy <yeremyvegap@gmail.com>
2025-11-06 22:32:26 -0500

    feat: relacion uno a muchos agregada (categorias 1-* libros)

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,15 +1,20 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from base import Base
 
 class Categoria(Base):
-    """tabla 'categoria'
+    """tabla 'categorias'
     - id_categoria: PRIMARY KEY
     - nombre : TEXT
+    - libros: relacion uno(categoria) a muchos (libros)
     """
     __tablename__ = "categoria"
 
     id_categoria = Column(Integer, primary_key=True, autoincrement=True)
     titulo = Column(String, nullable=False)
+
+    # relacion uno a muchos
+    libros = relationship("Libro", back_populates="categorias")
 
     def __repr__(self) -> str:
         return f"""<Categoria id_categoria ={self.id_categoria} nombre='{self.nombre}'>"""
