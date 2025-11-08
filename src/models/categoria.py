adfd34bef91aad38f54fd978158ae99fecd1b672
Yeremy <yeremyvegap@gmail.com>
2025-11-08 13:40:40 -0500

    feat: implementacion de descripcion. bugFix: arreglo de relacion

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,21 +1,23 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from base import Base
+from .base import Base
 
 class Categoria(Base):
     """tabla 'categorias'
     - id_categoria: PRIMARY KEY
     - nombre : TEXT
+    - descripcion : TEXT
     - libros: relacion uno(categoria) a muchos (libros)
     """
-    __tablename__ = "categoria"
+    __tablename__ = "categorias"
 
     id_categoria = Column(Integer, primary_key=True, autoincrement=True)
-    titulo = Column(String, nullable=False)
+    nombre = Column(String, nullable=False)
+    descripcion = Column(String, nullable=False)
 
     # relacion uno a muchos
-    libros = relationship("Libro", back_populates="categorias")
+    libros = relationship("Libro", back_populates="categoria")
 
     def __repr__(self) -> str:
-        return f"""<Categoria id_categoria ={self.id_categoria} nombre='{self.nombre}'>"""
+        return f"""<Categoria id_categoria ={self.id_categoria} nombre='{self.nombre}' descripcion='{self.descripcion}'>"""
     
