756098d29e950804723297e040a17af0cdf4ddb3
Yeremy <yeremyvegap@gmail.com>
2025-11-08 13:49:02 -0500

    feat: main menu donde su responsabilidad es llamar a sus submenus (escalable)

--- src/view/menu_principal.py
+++ src/view/menu_principal.py
@@ -1,1 +1,20 @@
-fro
+from view.menu_libro import menu_libros
+from view.menu_categoria import menu_categorias
+
+def main_menu():
+    while True:
+        print("\n=== GESTOR DE BIBLIOTECA ===")
+        print("1. Gestion de Libros")
+        print("2. Gestion de Categorías")
+        print("0. Salir")
+        op = input("Seleccione una opcion: ")
+
+        if op == "1":
+            menu_libros()
+        elif op == "2":
+            menu_categorias()
+        elif op == "0":
+            print("saliendo...")
+            break
+        else:
+            print("opcion invalida, intente de nuevo")
